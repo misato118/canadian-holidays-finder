d5ea91ab84dcc756a718d6cd2dd374ba44a21c92
misato118 <mtsz.29910@gmail.com>
2025-09-09 15:59:28 -0400

    Changed some stylings

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,60 +42,65 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <h1>Find Public Holidays in Canada</h1>
-      <form onSubmit={onSubmit}>
-      {/* Province dropdown */}
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn m-1">{province.title == "" ? "Choose Province" : province.title}</div>
-            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-              {provinces.map((province, index) => {
-                const title = province.substring(0, province.length - 3);
-                const code = province.substring(province.length - 2);
-                return (
-                  <li key={province + index} onClick={() => {setProvince({ title: title, code: code }); handleClick();}}><a>{title}</a></li>
-                )
-              })}
-            </ul>
+    <div className="min-h-[100dvh] flex flex-col bg-base-100">
+      <header className="sticky top-0 z-10 bg-base-100/90 backdrop-blur border-b">
+        <div className="mx-auto max-w-5xl px-4 py-10 text-center">
+          <h1 className="text-primary text-4xl font-bold">Find Public Holidays in Canada</h1>
+          <form onSubmit={onSubmit} className="pb-5 pt-10">
+            {/* Province dropdown */}
+            <div className="dropdown mr-5">
+              <div tabIndex={0} role="button" className="btn m-1 w-xs border-1 border-solid border-base-content">{province.title == "" ? "Choose Province" : province.title} ▼</div>
+              <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                {provinces.map((province, index) => {
+                  const title = province.substring(0, province.length - 3);
+                  const code = province.substring(province.length - 2);
+                  return (
+                    <li key={province + index} onClick={() => { setProvince({ title: title, code: code }); handleClick(); }}><a>{title}</a></li>
+                  )
+                })}
+              </ul>
+            </div>
+            {/* TODO: Date range selector here */}
+            <input type="date" className="mr-2 px-2 py-1 border-1 border-solid rounded-lg cursor-pointer" value={startDate} onChange={e => setStartDate(e.target.value)} />
+            <input type="date" className="ml-2 px-2 py-1 border-1 border-solid rounded-lg cursor-pointer" value={endDate} onChange={e => setEndDate(e.target.value)} />
+            {/* TODO: Search button here */}
+            <button className="btn ml-10 bg-accent text-info-content">
+              Search
+              <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <circle cx="11" cy="11" r="8"></circle>
+                  <path d="m21 21-4.3-4.3"></path>
+                </g>
+              </svg>
+            </button>
+          </form>
         </div>
-        {/* TODO: Date range selector here */}
-        <input type="date" value={startDate} onChange={e => setStartDate(e.target.value)} />
-        <input type="date" value={endDate} onChange={e => setEndDate(e.target.value)} />
-        {/* TODO: Search button here */}
-        <button className="btn">
-          Search
-          <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <g
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2.5"
-              fill="none"
-              stroke="currentColor"
-            >
-              <circle cx="11" cy="11" r="8"></circle>
-              <path d="m21 21-4.3-4.3"></path>
-            </g>
-          </svg>
-        </button>
-      </form>
+      </header>
 
-      {/* TODO: Display area here */}
-      <div>
-        {list ?
-        (
-          <ul>
-          {list.map((item, index) => {
-            return (
-              <li key={item.name + index}>{item.date} {item.name}</li>
-            );
-          })}
-        </ul>
-        )
-      : <p>Nothing yet</p>}
-
-       
-        {/* {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Nothing yet</p>} */}
-      </div>
+      <main className="flex-1 overflow-hidden px-4">
+        {/* Display area here */}
+        <div className="mx-auto max-w-xl py-6 space-y-4 h-[60vh]">
+          <div className="card bg-base-200 p-4 text-center min-h-full h-full overflow-y-auto">
+            {list ?
+              (
+                <ul>
+                {list.map((item, index) => {
+                  return (
+                    <li key={item.name + index}>{item.date} {item.name}</li>
+                  );
+                })}
+              </ul>
+              )
+            : <p>Nothing yet</p>}
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
